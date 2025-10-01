914cebe99255f9e62e0780cded1a994aaa98da28
angie0120 <angwilliams1@gmail.com>
2025-10-01 16:49:39 -0500

    Add failing policy gate on main (intentional violation)

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+﻿provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# Intentionally non-compliant: missing encryption, plus public-read ACL
+resource "aws_s3_bucket" "policy_violation_bucket" {
+  bucket = "circleci-lab-violation-${random_string.suffix.result}"
+}
+
+resource "aws_s3_bucket_acl" "policy_violation_bucket_acl" {
+  bucket = aws_s3_bucket.policy_violation_bucket.id
+  acl    = "public-read"
+}
